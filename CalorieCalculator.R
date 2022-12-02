ed015dce86ca18811a636809dbde625d06922353
www hotbabe com <ninabarkat@protonmail.com>
2022-12-02 12:40:03 +0100

    [PART-TWO] Finished

--- CalorieCalculator.R
+++ CalorieCalculator.R
@@ -1,5 +1,7 @@
 
-mostCalories = 0 
+numberOne = 0 
+numberTwo = 0
+numberThree = 0
 currentCalories = 0
 
 for (cal in readLines('./input.txt')) {
@@ -7,8 +9,12 @@ for (cal in readLines('./input.txt')) {
   ## if no, just skip
   cal = strtoi(cal) # Cast to integer
   if(is.na(cal) ){ # Newline
-    if(currentCalories > mostCalories) { # Check if current beats total
-      mostCalories =  currentCalories
+    if(currentCalories > numberOne) { # Check if current beats total
+      numberOne = currentCalories
+    } else if(currentCalories > numberTwo){
+      numberTwo = currentCalories
+    } else if(currentCalories > numberThree) {
+      numberThree = currentCalories
     }
     currentCalories = 0
   } else { # Normal run
@@ -20,6 +26,12 @@ for (cal in readLines('./input.txt')) {
 }
 
 print('Most calories carried by an elf: ')
-print(mostCalories)
+print(numberOne)
+print('Second most calories carried by an elf: ')
+print(numberTwo)
+print('Third most calories carried by an elf: ')
+print(numberThree)
 
+print('Total of top three: ')
+print(numberOne + numberTwo + numberThree)
 
